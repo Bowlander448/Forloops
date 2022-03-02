f40221822bcdad4792dc0dab87360430911e0011
Victor A. Quintana <bowlander448@gmail.com>
2022-03-02 09:43:19 -0400

    Initial Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,31 @@
+fun main(args: Array<String>) {
+    var things = mutableListOf<String>("Perro", "Gato")
+
+    for (each_thing in things) {
+        println(each_thing)
+    }
+
+    things.forEach {
+        println(it)
+    }
+
+    var dict = mutableMapOf<String, Float>("Darkest Dungeon 2" to 39.99f,
+    "Elden Ring" to 59.99f)
+
+    println(dict["Darkest Dungeon 2"])
+
+    dict["Minecraft"] = 0.0f
+    println("THe map is $dict")
+
+    dict.remove("Darkest Dungeon 2 ")
+    println("Now the map is $dict")
+
+    for ((key, value) in dict){
+        println("$key -> $value")
+    }
+
+    dict.forEach {
+        println("${it.key} -> ${it.value}")
+    }
+
+}
